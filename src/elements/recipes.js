3ab49d998c0f0da5bdee1c13a04c4e4f8ed45760
Do Tung Duong <duong98kk@gmail.com>
2019-10-08 12:10:56 +0700

    'add-lodash'

--- src/elements/recipes.js
+++ src/elements/recipes.js
@@ -3,6 +3,6 @@ export default [
   { Earth: { Earth: 'Pressure', Fire: 'Lava', Water: 'Mud', Rain: 'Plain', Lava: 'Volcano' } },
   { Fire: { Water: 'Steam', Plain: 'Tobacco', Mud: 'Brick' } },
   { Water: { Water: 'Sea', Sea: 'Ocean', Lava: 'Obsidian' } },
-  { Pressure: { Lava: 'Granite', Plain: 'Coal' } }
-
+  { Pressure: { Lava: 'Granite', Plain: 'Coal' } },
+  { Dust: {} }, { Energy: {} }, { Rain: {} }, { Lava: {} }, { Mud: {} }, { Steam: {} },{ Sea: {} },{ Atmosphere: {} },{ Wind: {} },{ Stone: {} },{ Plain: {} },{ Cloud: {} },{ Ocean: {} },{ Obsidian: {} },{ Tobacco: {} },{ Brick: {} },{ Volcano: {} },{ Granite: {} },{ Coal: {} }
 ]
